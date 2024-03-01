bc391b3c54b6ee9dce69f136d1b31285e166e2a5
Julian Wiesler <julian.wiesler@tngtech.com>
2024-03-01 20:06:31 +0100

    Write to out instead of file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 #![warn(clippy::pedantic)]
 
-use std::io::{BufReader, BufWriter, Write};
+use std::io::{stdout, BufReader, Write};
 use std::process::ExitCode;
 
 use times::format::Output;
 use times::parse::parse;
 
-use fs_err::{File, OpenOptions};
+use fs_err::File;
 use structopt::StructOpt;
 use thiserror::Error;
 
@@ -15,10 +15,6 @@ struct Command {
     /// Input path timesheet, defaults to `timesheet.txt`
     #[structopt(default_value = "timesheet.txt")]
     path: String,
-
-    /// Input path timesheet, defaults to `timesheet.txt`
-    #[structopt(default_value = "timesheet.out.txt")]
-    out: String,
 
     /// Whether to only check the input
     #[structopt(long)]
@@ -29,26 +25,19 @@ struct Command {
 enum Error {
     #[error("Failed to read input file: {0}")]
     InputFile(std::io::Error),
-    #[error("Failed to write to output file: {0}")]
-    OutputFile(std::io::Error),
     #[error("Failed to parse input: {0}")]
     Parse(#[from] times::parse::Error),
     #[error("Invalid times: {0}")]
     Validate(#[from] times::verify::Error),
 }
 
-fn run(Command { path, out, check }: Command) -> Result<(), Error> {
+fn run(Command { path, check }: Command) -> Result<(), Error> {
     let file = File::open(path).map_err(Error::InputFile)?;
     let days = parse(&mut BufReader::new(file))?;
     let output = Output::new(&days)?;
 
     if !check {
-        let out = OpenOptions::new()
-            .write(true)
-            .create(true)
-            .open(out)
-            .map_err(Error::OutputFile)?;
-        write!(&mut BufWriter::new(out), "{output}").expect("format output");
+        write!(&mut stdout(), "{output}").expect("format output");
     }
     Ok(())
 }
