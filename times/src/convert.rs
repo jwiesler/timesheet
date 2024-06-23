2b5ee79dc5b483d739636d8e2b00bc5b80747295
Julian Wiesler <julian.wiesler@ext.aleph-alpha.com>
2024-06-23 09:11:09 +0200

    Travel times overlap times calculation

--- times/src/convert.rs
+++ times/src/convert.rs
@@ -3,14 +3,18 @@ use thiserror::Error;
 
 use crate::{Minutes, Positioned, Time, Topic};
 
-#[derive(Debug, Error)]
+#[derive(Debug, Error, Eq, PartialEq)]
 pub enum Error {
     #[error("Time span in line {0} is never terminated")]
     NotTerminated(usize),
     #[error("Minutes of time in line {0} are not a multiple of three")]
     TimeNotMultipleOfThree(usize),
     #[error("Time in line {0} ends before it starts")]
-    UnorderedEntries(usize),
+    EndsBeforeItStarts(usize),
+    #[error("Time in line {0} overlaps with the time before it")]
+    OverlapWithPrevious(usize),
+    #[error("Time in line {0} crosses the start of end of a previous travel time")]
+    AcrossTravelTime(usize),
 }
 
 #[cfg_attr(test, derive(Default, Eq, PartialEq))]
@@ -31,11 +35,15 @@ pub struct Day {
 
 #[must_use]
 fn accumulated_time<'a>(entries: impl IntoIterator<Item = &'a Entry>) -> AccumulatedTime {
+    let mut last_travel = None;
     entries
         .into_iter()
         .fold(AccumulatedTime::default(), |acc, entry| {
             let AccumulatedTime { travel, work } = acc;
             let duration = entry.duration;
+            if entry.identifier.ends_with("Fa") {
+                last_travel = Some(entry);
+            }
             if entry.identifier.starts_with("Ustd") {
                 AccumulatedTime { travel, work }
             } else if &entry.identifier == "TNGFa" {
@@ -44,12 +52,47 @@ fn accumulated_time<'a>(entries: impl IntoIterator<Item = &'a Entry>) -> Accumul
                     work,
                 }
             } else {
-                AccumulatedTime {
-                    work: work + duration,
-                    travel,
+                if last_travel
+                    .filter(|t| {
+                        t.start.value <= entry.start.value && entry.end.value <= t.end.value
+                    })
+                    .is_some()
+                {
+                    AccumulatedTime {
+                        work: work + duration,
+                        travel: travel - duration,
+                    }
+                } else {
+                    AccumulatedTime {
+                        work: work + duration,
+                        travel,
+                    }
                 }
             }
         })
+}
+
+fn validate_ordering(
+    entry: &Entry,
+    previous: &Entry,
+    last_travel: Option<&Entry>,
+) -> Result<(), Error> {
+    if let Some(last_travel) = last_travel {
+        if entry.start.value < last_travel.start.value
+            || (entry.start.value < last_travel.end.value
+                && last_travel.end.value < entry.end.value)
+        {
+            return Err(Error::AcrossTravelTime(entry.start.line));
+        }
+        if std::ptr::eq(last_travel, previous) {
+            return Ok(());
+        }
+    }
+    if previous.end.value <= entry.start.value {
+        Ok(())
+    } else {
+        Err(Error::OverlapWithPrevious(entry.start.line))
+    }
 }
 
 impl TryFrom<crate::Day> for Day {
@@ -61,7 +104,8 @@ impl TryFrom<crate::Day> for Day {
             day,
             entries,
         } = value;
-        let mut new_entries = Vec::with_capacity(entries.len());
+        let mut new_entries: Vec<Positioned<Entry>> = Vec::with_capacity(entries.len());
+        let mut last_travel = None;
         let mut iter = entries.into_iter().peekable();
         while let Some(entry) = iter.next() {
             if entry.value.time.minute % 3 != 0 {
@@ -77,17 +121,29 @@ impl TryFrom<crate::Day> for Day {
                     .value
                     .time
                     .elapsed(entry.value.time)
-                    .ok_or(Error::UnorderedEntries(entry.line))?;
-                new_entries.push(Positioned::new(
-                    entry.line,
-                    Entry {
-                        start: Positioned::new(entry.line, entry.value.time),
-                        end: Positioned::new(next.line, next.value.time),
-                        duration,
-                        identifier,
-                        comment,
-                    },
-                ));
+                    .ok_or(Error::EndsBeforeItStarts(entry.line))?;
+                let new_entry = Entry {
+                    start: Positioned::new(entry.line, entry.value.time),
+                    end: Positioned::new(next.line, next.value.time),
+                    duration,
+                    identifier,
+                    comment,
+                };
+
+                if new_entry.identifier.ends_with("Fa") {
+                    last_travel = Some(new_entries.len());
+                }
+                if let Some(previous_entry) = new_entries.last() {
+                    validate_ordering(
+                        &new_entry,
+                        &previous_entry.value,
+                        last_travel
+                            .and_then(|i: usize| new_entries.get(i))
+                            .map(|e| &e.value),
+                    )?;
+                }
+
+                new_entries.push(Positioned::new(entry.line, new_entry));
             }
         }
 
@@ -152,11 +208,13 @@ impl Add<AccumulatedTime> for AccumulatedTime {
 
 #[cfg(test)]
 mod test {
-    use crate::convert::{accumulated_time, billable_travel_time, AccumulatedTime, Entry};
-    use crate::Minutes;
+    use crate::convert::{
+        accumulated_time, billable_travel_time, validate_ordering, AccumulatedTime, Entry, Error,
+    };
+    use crate::{Minutes, Positioned, Time};
 
     #[test]
-    fn test_travel_time_calc() {
+    fn travel_time_calc() {
         assert_eq!(billable_travel_time(Minutes::from(20)), Minutes::default());
         assert_eq!(
             billable_travel_time(Minutes::from(4 * 60 + 45)),
@@ -168,47 +226,124 @@ mod test {
         );
     }
 
+    fn new_entry(start: Option<Time>, end: Option<Time>, identifier: &str) -> Entry {
+        Entry {
+            start: Positioned::new(0, start.unwrap()),
+            end: Positioned::new(0, end.unwrap()),
+            duration: end.unwrap().elapsed(start.unwrap()).unwrap(),
+            identifier: identifier.to_string(),
+            comment: None,
+        }
+    }
+
     #[test]
-    fn test_accumulated_time() {
+    fn accumulated_travel_time() {
         let entries = [
-            Entry {
-                duration: 60.into(),
-                identifier: "TNG".to_string(),
-                ..Default::default()
-            },
-            Entry {
-                duration: 30.into(),
-                identifier: "TNGFa".to_string(),
-                ..Default::default()
-            },
-            Entry {
-                duration: 60.into(),
-                identifier: "TNG".to_string(),
-                ..Default::default()
-            },
-            Entry {
-                duration: 30.into(),
-                identifier: "Ustd".to_string(),
-                ..Default::default()
-            },
-            Entry {
-                duration: 30.into(),
-                identifier: "UstdPart".to_string(),
-                ..Default::default()
-            },
-            Entry {
-                duration: 30.into(),
-                identifier: "TNGFa".to_string(),
-                ..Default::default()
-            },
+            new_entry(Time::new(0, 0), Time::new(1, 0), "TNG"),
+            new_entry(Time::new(1, 0), Time::new(1, 30), "TNGFa"),
+            new_entry(Time::new(1, 30), Time::new(2, 30), "TNG"),
+        ];
+
+        assert_eq!(
+            accumulated_time(&entries),
+            AccumulatedTime {
+                travel: 30.into(),
+                work: 120.into(),
+            }
+        );
+    }
+
+    #[test]
+    fn test_accumulated_under_hours() {
+        let entries = [
+            new_entry(Time::new(1, 30), Time::new(2, 0), "TNG"),
+            new_entry(Time::new(2, 0), Time::new(2, 30), "TNGFa"),
+            new_entry(Time::new(2, 30), Time::new(3, 0), "Ustd"),
+            new_entry(Time::new(3, 0), Time::new(3, 30), "UstdPart"),
+            new_entry(Time::new(3, 30), Time::new(4, 0), "TNGFa"),
         ];
 
         assert_eq!(
             accumulated_time(&entries),
             AccumulatedTime {
                 travel: 60.into(),
-                work: 120.into()
+                work: 30.into(),
             }
+        );
+    }
+
+    #[test]
+    fn accumulated_overlapping_travel_times() {
+        let entries = [
+            new_entry(Time::new(3, 0), Time::new(4, 0), "TNG"),
+            new_entry(Time::new(4, 0), Time::new(4, 30), "TNGFa"),
+            new_entry(Time::new(4, 0), Time::new(4, 10), "TNG"),
+            new_entry(Time::new(4, 15), Time::new(4, 20), "TNG"),
+            new_entry(Time::new(4, 20), Time::new(4, 30), "TNG"),
+        ];
+
+        assert_eq!(
+            accumulated_time(&entries),
+            AccumulatedTime {
+                travel: 5.into(),
+                work: 85.into(),
+            }
+        );
+    }
+
+    #[test]
+    fn ordering() {
+        let previous_entry = new_entry(Time::new(1, 0), Time::new(2, 0), "TNG");
+        let entry = new_entry(Time::new(2, 0), Time::new(2, 30), "TNG");
+        assert_eq!(validate_ordering(&entry, &previous_entry, None), Ok(()));
+
+        let entry = new_entry(Time::new(1, 59), Time::new(2, 30), "TNG");
+        assert_eq!(
+            validate_ordering(&entry, &previous_entry, None),
+            Err(Error::OverlapWithPrevious(0))
+        );
+    }
+
+    #[test]
+    fn ordering_travel() {
+        let travel = new_entry(Time::new(1, 0), Time::new(2, 30), "TNGFa");
+
+        // After travel
+        let entry = new_entry(Time::new(2, 30), Time::new(3, 00), "TNG");
+        assert_eq!(validate_ordering(&entry, &travel, Some(&travel)), Ok(()));
+
+        // Overlaps and fully inside
+        let entry = new_entry(Time::new(1, 30), Time::new(2, 00), "TNG");
+        assert_eq!(validate_ordering(&entry, &travel, Some(&travel)), Ok(()));
+
+        // Overlaps and starts before travel
+        let entry = new_entry(Time::new(0, 30), Time::new(1, 30), "TNG");
+        assert_eq!(
+            validate_ordering(&entry, &travel, Some(&travel)),
+            Err(Error::AcrossTravelTime(0))
+        );
+
+        // Overlaps and ends after travel
+        let entry = new_entry(Time::new(2, 0), Time::new(3, 0), "TNG");
+        assert_eq!(
+            validate_ordering(&entry, &travel, Some(&travel)),
+            Err(Error::AcrossTravelTime(0))
+        );
+
+        let previous_entry = new_entry(Time::new(1, 30), Time::new(2, 0), "TNG");
+
+        // Inside travel and after previous
+        let entry = new_entry(Time::new(2, 00), Time::new(2, 30), "TNG");
+        assert_eq!(
+            validate_ordering(&entry, &previous_entry, Some(&travel)),
+            Ok(())
+        );
+
+        // Inside travel and overlaps previous
+        let entry = new_entry(Time::new(1, 45), Time::new(2, 30), "TNG");
+        assert_eq!(
+            validate_ordering(&entry, &previous_entry, Some(&travel)),
+            Err(Error::OverlapWithPrevious(0))
         );
     }
 }
