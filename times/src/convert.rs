2c0104f3cbc28c77458a0ee66162bcce2655f8ca
Julian Wiesler <julian.wiesler@ext.aleph-alpha.com>
2024-07-20 15:25:34 +0200

    Fix issue with non tng travel times

--- times/src/convert.rs
+++ times/src/convert.rs
@@ -1,3 +1,4 @@
+use std::fmt::{Display, Formatter};
 use std::ops::Add;
 
 use thiserror::Error;
@@ -19,11 +20,37 @@ pub enum Error {
 }
 
 #[cfg_attr(test, derive(Default, Eq, PartialEq))]
+pub struct Identifier(String);
+
+impl Display for Identifier {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl Identifier {
+    #[must_use]
+    pub fn is_tng(&self) -> bool {
+        self.0.starts_with("TNG")
+    }
+
+    #[must_use]
+    pub fn is_travel(&self) -> bool {
+        self.0.ends_with("Fa")
+    }
+
+    #[must_use]
+    pub fn is_under_hours(&self) -> bool {
+        self.0.starts_with("Ustd")
+    }
+}
+
+#[cfg_attr(test, derive(Default, Eq, PartialEq))]
 pub struct Entry {
     pub start: Positioned<Time>,
     pub end: Positioned<Time>,
     pub duration: Minutes,
-    pub identifier: String,
+    pub identifier: Identifier,
     pub comment: Option<String>,
 }
 
@@ -53,23 +80,42 @@ fn accumulated_time<'a>(entries: impl IntoIterator<Item = &'a Entry>) -> Accumul
         .fold(AccumulatedTime::default(), |acc, entry| {
             let AccumulatedTime { travel, work } = acc;
             let duration = entry.duration;
-            if entry.identifier.ends_with("Fa") {
+            if entry.identifier.is_travel() {
                 last_travel = Some(entry);
             }
-            if entry.identifier.starts_with("Ustd") {
+            if entry.identifier.is_under_hours() {
                 AccumulatedTime { travel, work }
-            } else if &entry.identifier == "TNGFa" {
-                AccumulatedTime {
-                    travel: travel + duration,
-                    work,
-                }
-            } else if last_travel
+            } else if entry.identifier.is_travel() {
+                let travel = if entry.identifier.is_tng() {
+                    TravelTime {
+                        tng: travel.tng + duration,
+                        other: travel.other,
+                    }
+                } else {
+                    TravelTime {
+                        tng: travel.tng,
+                        other: travel.other + duration,
+                    }
+                };
+                AccumulatedTime { travel, work }
+            } else if let Some(last_travel) = last_travel
                 .filter(|t| t.start.value <= entry.start.value && entry.end.value <= t.end.value)
-                .is_some()
             {
+                let travel = if last_travel.identifier.is_tng() {
+                    TravelTime {
+                        tng: travel.tng - duration,
+                        other: travel.other,
+                    }
+                } else {
+                    TravelTime {
+                        tng: travel.tng,
+                        other: travel.other - duration,
+                    }
+                };
+
                 AccumulatedTime {
                     work: work + duration,
-                    travel: travel - duration,
+                    travel,
                 }
             } else {
                 AccumulatedTime {
@@ -124,6 +170,7 @@ impl TryFrom<crate::Day> for Day {
                 comment,
             } = entry.value.topic
             {
+                let identifier = Identifier(identifier);
                 let next = iter.peek().ok_or(Error::NotTerminated(entry.line))?;
                 let duration = next
                     .value
@@ -138,7 +185,7 @@ impl TryFrom<crate::Day> for Day {
                     comment,
                 };
 
-                if new_entry.identifier.ends_with("Fa") {
+                if new_entry.identifier.is_travel() {
                     last_travel = Some(new_entries.len());
                 }
                 if let Some(previous_entry) = new_entries.last() {
@@ -167,13 +214,35 @@ impl TryFrom<crate::Day> for Day {
 
 #[derive(Default, Clone)]
 #[cfg_attr(test, derive(Debug, Eq, PartialEq))]
-pub struct AccumulatedTime {
-    travel: Minutes,
-    work: Minutes,
+struct TravelTime {
+    tng: Minutes,
+    other: Minutes,
 }
 
-fn billable_travel_time(travel: Minutes) -> Minutes {
-    let minutes = travel.into_inner();
+impl Add for TravelTime {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        TravelTime {
+            tng: self.tng + rhs.tng,
+            other: self.other + rhs.other,
+        }
+    }
+}
+
+impl TravelTime {
+    fn billable(&self) -> Minutes {
+        let tng = billable_travel_time(self.tng);
+        tng + self.other
+    }
+
+    fn total(&self) -> Minutes {
+        self.tng + self.other
+    }
+}
+
+fn billable_travel_time(minutes: Minutes) -> Minutes {
+    let minutes = minutes.into_inner();
     if let Some(m) = minutes.checked_sub(45) {
         Minutes::from(((m * 3) / 4).min(6 * 60))
     } else {
@@ -181,15 +250,22 @@ fn billable_travel_time(travel: Minutes) -> Minutes {
     }
 }
 
+#[derive(Default, Clone)]
+#[cfg_attr(test, derive(Debug, Eq, PartialEq))]
+pub struct AccumulatedTime {
+    travel: TravelTime,
+    work: Minutes,
+}
+
 impl AccumulatedTime {
     #[must_use]
     pub fn billable_travel_time(&self) -> Minutes {
-        billable_travel_time(self.travel)
+        self.travel.billable()
     }
 
     #[must_use]
     pub fn travel_time(&self) -> Minutes {
-        self.travel
+        self.travel.total()
     }
 
     #[must_use]
@@ -218,7 +294,8 @@ impl Add<AccumulatedTime> for AccumulatedTime {
 mod test {
     use crate::{Minutes, Positioned, Time};
     use crate::convert::{
-        accumulated_time, AccumulatedTime, billable_travel_time, Entry, Error, validate_ordering,
+        accumulated_time, AccumulatedTime, billable_travel_time, Entry, Error, Identifier,
+        TravelTime, validate_ordering,
     };
 
     #[test]
@@ -239,7 +316,7 @@ mod test {
             start: Positioned::new(0, start.unwrap()),
             end: Positioned::new(0, end.unwrap()),
             duration: end.unwrap().elapsed(start.unwrap()).unwrap(),
-            identifier: identifier.to_string(),
+            identifier: Identifier(identifier.to_string()),
             comment: None,
         }
     }
@@ -255,7 +332,10 @@ mod test {
         assert_eq!(
             accumulated_time(&entries),
             AccumulatedTime {
-                travel: 30.into(),
+                travel: TravelTime {
+                    tng: 30.into(),
+                    other: 0.into()
+                },
                 work: 120.into(),
             }
         );
@@ -274,7 +354,10 @@ mod test {
         assert_eq!(
             accumulated_time(&entries),
             AccumulatedTime {
-                travel: 60.into(),
+                travel: TravelTime {
+                    tng: 60.into(),
+                    other: 0.into()
+                },
                 work: 30.into(),
             }
         );
@@ -288,13 +371,18 @@ mod test {
             new_entry(Time::new(4, 0), Time::new(4, 10), "TNG"),
             new_entry(Time::new(4, 15), Time::new(4, 20), "TNG"),
             new_entry(Time::new(4, 20), Time::new(4, 30), "TNG"),
+            new_entry(Time::new(5, 0), Time::new(5, 30), "AAFa"),
+            new_entry(Time::new(5, 0), Time::new(5, 15), "AA"),
         ];
 
         assert_eq!(
             accumulated_time(&entries),
             AccumulatedTime {
-                travel: 5.into(),
-                work: 85.into(),
+                travel: TravelTime {
+                    tng: 5.into(),
+                    other: 15.into(),
+                },
+                work: 100.into(),
             }
         );
     }
