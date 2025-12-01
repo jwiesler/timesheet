31eed2d3027d40b9263654f85c10f625d4b40a74
Julian Wiesler <julian.wiesler@ext.aleph-alpha.com>
2025-12-01 11:17:24 +0100

    Update templates

--- times/src/generate.rs
+++ times/src/generate.rs
@@ -19,7 +19,6 @@ pub enum Template {
     Holiday,
     Normal,
     Ill,
-    TNGWeekly,
 }
 
 trait FormatterEx {
@@ -79,30 +78,6 @@ impl Template {
                 write_with(&mut output, |f| {
                     f.header(date)?;
                     writeln!(f, "09:00 AA {arg_0}")?;
-                    writeln!(f, "10:00 AA Ops Daily")?;
-                    writeln!(f, "10:15 AA Inference Daily")?;
-                    writeln!(f, "10:30 AA {arg_0}")?;
-                    writeln!(f, "12:30")?;
-                    writeln!(f, "13:00 AA {arg_1}")?;
-                    writeln!(f, "17:30")
-                })
-                .unwrap();
-            }
-            Template::TNGWeekly => {
-                if args.is_empty() || 2 < args.len() {
-                    return Err(Error::Argc(2, args.len()));
-                }
-                let arg_0 = &args[0];
-                let arg_1 = args.get(1).unwrap_or(arg_0);
-
-                write_with(&mut output, |f| {
-                    f.header(date)?;
-                    writeln!(f, "09:00 AA {arg_0}")?;
-                    writeln!(f, "10:00 AA Ops Daily")?;
-                    writeln!(f, "10:15 AA Inference Daily")?;
-                    writeln!(f, "10:30 AANB TNG Weekly")?;
-                    writeln!(f, "10:36 AA TNG Weekly")?;
-                    writeln!(f, "11:00 AA {arg_0}")?;
                     writeln!(f, "12:30")?;
                     writeln!(f, "13:00 AA {arg_1}")?;
                     writeln!(f, "17:30")
@@ -141,12 +116,27 @@ mod tests {
         let date = Date::new(NaiveDate::from_ymd_opt(2024, 8, 5).unwrap());
         let tests = [
             (Template::Empty, vec![], "\n* Mo. 5.08.\n"),
-            (Template::TechDay, vec![], "\n* Mo. 5.08.\n09:00 TNGFo Techday\n17:00\n"),
-            (Template::Holiday, vec![], "\n* Mo. 5.08.\n09:00 Urlaub\n17:00\n"),
-            (Template::TNGWeekly, vec!["A"], "\n* Mo. 5.08.\n09:00 AA A\n10:00 AA Ops Daily\n10:15 AA Inference Daily\n10:30 AANB TNG Weekly\n10:36 AA TNG Weekly\n11:00 AA A\n12:30\n13:00 AA A\n17:30\n"),
-            (Template::Normal, vec!["A"], "\n* Mo. 5.08.\n09:00 AA A\n10:00 AA Ops Daily\n10:15 AA Inference Daily\n10:30 AA A\n12:30\n13:00 AA A\n17:30\n"),
-            (Template::Normal, vec!["A", "B"], "\n* Mo. 5.08.\n09:00 AA A\n10:00 AA Ops Daily\n10:15 AA Inference Daily\n10:30 AA A\n12:30\n13:00 AA B\n17:30\n"),
-            (Template::Ill, vec![], "\n* Mo. 5.08.\n09:00 Krank\n17:00\n")
+            (
+                Template::TechDay,
+                vec![],
+                "\n* Mo. 5.08.\n09:00 TNGFo Techday\n17:00\n",
+            ),
+            (
+                Template::Holiday,
+                vec![],
+                "\n* Mo. 5.08.\n09:00 Urlaub\n17:00\n",
+            ),
+            (
+                Template::Normal,
+                vec!["A"],
+                "\n* Mo. 5.08.\n09:00 AA A\n12:30\n13:00 AA A\n17:30\n",
+            ),
+            (
+                Template::Normal,
+                vec!["A", "B"],
+                "\n* Mo. 5.08.\n09:00 AA A\n12:30\n13:00 AA B\n17:30\n",
+            ),
+            (Template::Ill, vec![], "\n* Mo. 5.08.\n09:00 Krank\n17:00\n"),
         ];
         for (template, args, result) in tests {
             assert_eq!(
